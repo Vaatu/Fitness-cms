95b2b5fdec07be8d242b42bd61fa3e8799e8de02
Vaatu <muh.elarabi@gmail.com>
2023-07-11 05:50:26 +0300

    cleanup app.js, separate swagger.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const { swaggerUi, swaggerDocs } = require('./swagger');
+
 const coachRouter = require('./routes/coachRouter');
 const clientRouter = require('./routes/clientRouter');
 const app = express();
@@ -10,36 +10,22 @@ const port = 3000;
 
 const sequelize = require('./utils/database').sequelize;
 const associations = require('./associations');
-associations(sequelize); // Pass the Sequelize instance to the associations function
+associations(sequelize); 
 
-sequelize.sync({ alter: process.env.DB_ALTER, force: process.env.DB_FORCE_DROP })
-  .then(() => {
-    console.log('Models synchronized with the database');
-  })
-  .catch((error) => {
-    console.error('Error synchronizing models:', error);
-  });
+// sequelize.sync({ alter: process.env.DB_ALTER, force: process.env.DB_FORCE_DROP })
+//   .then(() => {
+//     console.log('Models synchronized with the database');
+//   })
+//   .catch((error) => {
+//     console.error('Error synchronizing models:', error);
+//   });
 
 // Middleware
 app.use(express.json());
 
-// Swagger configuration
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Coach Management API',
-      description: 'API documentation for managing coaches and clients',
-      version: '1.0.0',
-    },
-  },
-  apis: ['./routes/*.js'],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/coaches', coachRouter);
 app.use('/clients', clientRouter);
 
---
+++ app/swagger.js
@@ -0,0 +1,18 @@
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Coach Management API',
+      description: 'API documentation for managing coaches and clients',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+module.exports = { swaggerUi, swaggerDocs };
